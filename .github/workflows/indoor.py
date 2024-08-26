30dc0b13449c1a460b1a0c62ff1de73644938526
dizastock <diza@stockwell.tv>
2024-08-26 15:32:19 -0400

    Add files via upload

---
+++ .github/workflows/indoor.py
@@ -0,0 +1,14 @@
+# indoor.py
+
+def main():
+    # Prompt the user for input
+    user_input = input("DizaStock: Please enter your text==>> ")
+
+    # Convert the input to lowercase
+    lowercase_input = user_input.lower()
+
+    # Output the lowercase input
+    print(lowercase_input)
+
+if __name__ == "__main__":
+    main()
